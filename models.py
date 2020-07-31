24ea2b1dbc5dd89e424437c9ce56ce6467fbda72
Jose Terrera <joseterrera@yahoo.com>
2020-07-30 21:17:11 -0700

    moving forward with model

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+class User(db.model):
+  """Site User"""
+
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key = True)
+  first_name = db.Column(db.Text, nullable=False)
+  last_name = db.Column(db.Text, nullable=False)
+  image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+  @property
+  def full_name(self):
+    """Return full name of user"""
+    return f"( {self.first_name} {self.last_name} )"
+
+
+def connect_db(app):
+  """Connect this database to provided flask app.
+  Call this in your flask app"""
+
+  db.app = app
+  db.init_app(app)
